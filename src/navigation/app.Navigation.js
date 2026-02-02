b7b688b6ed5dfc4a7059d2c1fba754137b32c7dc
JordanGuevaraChalial <jaguevara11@espe.edu.ec>
2026-02-02 12:31:50 -0500

    Cambios entre el uso de AuthContext

--- src/navigation/app.Navigation.js
+++ src/navigation/app.Navigation.js
@@ -1,18 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 import { HomeScreen } from '../screens/home/home';
 import { AutorScreen } from '../screens/autor/autor';
 import { LibroScreen } from '../screens/libro/libro';
 import { AutorFormScreen } from '../screens/autor/autor-form';
 import { createStackNavigator } from '@react-navigation/stack';
-
-
+import {AuthContext} from "../context/AuthContext"
+import { useContext } from 'react';
 const Stack = createStackNavigator();
 
 export const AppStack=()=> {
+  const {signOut}=useContext(AuthContext);
   return (
     <NavigationContainer >
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} options={{title:"Inicio"}}/>
+            <Stack.Screen name='Home' component={HomeScreen}
+            options={{title:"Inicio", headerRight:()=>(<Button title="Cerrar Sesión" 
+            onPress={()=>Alert.alert("Alerta", "¿Estás seguro de que quieres cerrar sesión?", 
+            [
+              {text:"Cancelar"}, 
+              {text:"Cerrar", onPress:signOut}
+            ] 
+              )}/>
+            )}}/>
             <Stack.Screen name='Autor' component={AutorScreen} />
             <Stack.Screen name='Libro' component={LibroScreen}/>
             <Stack.Screen name='AutorForm' component={AutorFormScreen} options={{title:"Crear Autor"}}/>
